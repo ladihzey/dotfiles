c39d7a4d4d08db3b1c8053a2e10db4adca64633a
ladihzey <yahor.zhydal@itechart-group.com>
2022-03-13 03:16:50 +0500

    Clear up configs and set up a new tools after machine change

--- install.sh
+++ install.sh
@@ -1,15 +1,12 @@
 # Variables
-DOTFILES_DIR=~/dotfiles
+DOTFILES=~/dotfiles
 
 # Create symlinks
-mkdir -p ~/.config/nvim
 mkdir -p ~/.config/alacritty
 
-ln -sf ${DOTFILES_DIR}/vim/init.vim           ~/.config/nvim/init.vim
-ln -sf ${DOTFILES_DIR}/terminal/alacritty.yml ~/.config/alacritty/alacritty.yml
-ln -sf ${DOTFILES_DIR}/zsh/zshrc              ~/.zshrc
-ln -sf ${DOTFILES_DIR}/git/gitconfig          ~/.gitconfig
-ln -sf ${DOTFILES_DIR}/terminal/dir_colors    ~/.dir_colors
-
-# Set zsh as the default shell
-sudo chsh -s /bin/zsh
+ln -sf ${DOTFILES}/tools/alacritty.yml      ~/.config/alacritty/alacritty.yml
+ln -sf ${DOTFILES}/tools/gitconfig          ~/.gitconfig
+ln -sf ${DOTFILES}/tools/config.fish        ~/.config/fish/config.fish
+ln -sf ${DOTFILES}/tools/starship.toml      ~/.config/startship.toml
+ls -sf ${DOTFILES}/vscode/settings.json     ~/.config/Code/User/settings.json
+ls -sf ${DOTFILES}/vscode/keybindings.json  ~/.config/Code/User/keybindings.json
