8dd6c3d3bc51e028dea240c629e5528d7ea8a252
ladihzey <yahor.zhydal@itechart-group.com>
2022-03-13 11:51:28 +0500

    Add fish initialization script

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ mkdir -p ~/.config/alacritty
 
 ln -sf ${DOTFILES}/tools/alacritty.yml      ~/.config/alacritty/alacritty.yml
 ln -sf ${DOTFILES}/tools/gitconfig          ~/.gitconfig
-ln -sf ${DOTFILES}/tools/config.fish        ~/.config/fish/config.fish
 ln -sf ${DOTFILES}/tools/starship.toml      ~/.config/startship.toml
+ln -sf ${DOTFILES}/fish/config.fish         ~/.config/fish/config.fish
 ls -sf ${DOTFILES}/vscode/settings.json     ~/.config/Code/User/settings.json
 ls -sf ${DOTFILES}/vscode/keybindings.json  ~/.config/Code/User/keybindings.json
