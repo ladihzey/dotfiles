53064583cafe3f44479d6cdd142874179bfda453
ladihzey <yahor.zhydal@itechart-group.com>
2022-03-14 02:12:09 +0500

    Set PATH from fish config and switch to NVM

--- fish/config.fish
+++ fish/config.fish
@@ -1,16 +1,12 @@
-if status is-interactive
-    # Commands to run in interactive sessions can go here
-end
+# Variables
+set -e fish_user_paths
+set -U fish_user_paths $HOME/.local/bin $HOME/.cargo/bin $HOME/bin $fish_user_paths
 
 # Settings ====================================================================
 set fish_greeting
-fish_add_path $VOLTA_HOME/bin
-fish_add_path $HOME/.cargo/bin
-fish_add_path $HOME/bin
 
 # Utilities ===================================================================
 starship init fish | source
-set -gx VOLTA_HOME "$HOME/.volta"
 
 # Aliases =====================================================================
 alias md="mkdir"
--- fish/install.sh
+++ fish/install.sh
@@ -1,2 +1,6 @@
-curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher
+curl -sL https://git.io/fisher | source
+
+fisher install jorgebucaran/fisher
+fisher install jorgebucaran/nvm.fish
 fisher install andreiborisov/sponge
+fisher install danhper/fish-ssh-agent
