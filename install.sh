a16275dc45cd4df7bb3e2d36ffc9bc42fa1bc3ff
ladihzey <ladihzey@proton.me>
2023-04-13 01:55:06 +0200

    feat: set-up vscode settings and extensions

--- install.sh
+++ install.sh
@@ -21,8 +21,10 @@ nix-env -iA \
 
 # Install asdf
 git clone https://github.com/asdf-vm/asdf.git ~/.asdf
-mkdir -p ~/.config/fish/completions; and ln -s ~/.asdf/completions/asdf.fish ~/.config/fish/completions
+mkdir -p ~/.config/fish/completions
+ln -s ~/.asdf/completions/asdf.fish ~/.config/fish/completions
 
+# Install NodeJS
 asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
 asdf install nodejs latest
 asdf global nodejs latest
@@ -32,3 +34,22 @@ curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
 # Link .dotfiles
 stow config
+
+# Set fish as a default shell
+echo $(which fish) | sudo tee -a /etc/shells
+chsh -s $(which fish)
+
+# Install VSCode extensions
+code --install-extensions \
+    arcticicestudio.nord-visual-studio-code \
+    bmalehorn.vscode-fish \
+    bungcip.better-toml \
+    EditorConfig.EditorConfig \
+    helgardrichard.helium-icon-theme \
+    rust-lang.rust-analyzer \
+    streetsidesoftware.code-spell-checker \
+    streetsidesoftware.code-spell-checker-russian \
+    vadimcn.vscode-lldb \
+    vscodevim.vim \
+    VSpaceCode.whichkey
+
